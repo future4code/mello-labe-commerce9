82ac947131541a026b86d2ef14714e81b67b8e91
Diego <di__e__go@hotmail.com>
2020-05-29 16:25:26 -0300

    Filtro crescente e decrescente

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-
-import React from 'react';
-import styled from 'styled-components';
-import Produtos from './componentes/Produtos';
-import CarrinhoDeCompras from './componentes/CarrinhoDeCompras';
+import React from "react";
+import styled from "styled-components";
+import Produtos from "./componentes/Produtos";
+import CarrinhoDeCompras from "./componentes/CarrinhoDeCompras";
 import carrinho from "./imagens/carrinho1.jpg";
 import Filtros from "./componentes/Filtros";
 import Carrinho from "./componentes/Carrinho";
 
-const AppContainer = styled.div `
+const AppContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-`
+`;
 
-const Header = styled.div `
+const Header = styled.div`
   width: 97.7%;
   height: 200px;
   padding-left: 30px;
   color: white;
-  background-color: #8B008B;
-`
+  background-color: #8b008b;
+`;
 
-const InputFiltros = styled.input `
+const InputFiltros = styled.input`
   padding: 3px;
   margin-left: 5px;
   margin-right: 15px;
-`
+`;
 
-const ContainerProdutos = styled.div `
+const ContainerProdutos = styled.div`
   display: flex;
   flex-flow: row wrap;
   justify-content: space-around;
@@ -35,26 +34,31 @@ const ContainerProdutos = styled.div `
   width: 98%;
   height: 740px;
   padding: 10px;
-`
-const Container = styled.div `
+`;
+const Container = styled.div`
   display: flex;
   flex-direction: row;
-`
+`;
 
-const ContainerCarrinho = styled.div `
+const ContainerCarrinho = styled.div`
   display: flex;
   justify-content: flex-end;
   width: 100%;
   position: fixed;
   margin-top: 560px;
-`
+`;
 
-const Imagem = styled.img `
+const Imagem = styled.img`
   height: 80px;
   width: 80px;
   margin-right: 20px;
   border-radius: 50px;
-`
+`;
+const ContainerSelect = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  height: 2vh;
+`;
 
 class App extends React.Component {
   state = {
@@ -62,101 +66,123 @@ class App extends React.Component {
       {
         id: 1,
         nome: "Camiseta Nasa",
-        valor: 149.90,
-        foto: "https://picsum.photos/260/200?a=1"
+        valor: 149.9,
+        foto: "https://picsum.photos/260/200?a=1",
       },
       {
         id: 2,
         nome: "Relógio de pulso Nasa",
-        valor: 297.50,
-        foto: "https://picsum.photos/260/200?a=2"
+        valor: 297.5,
+        foto: "https://picsum.photos/260/200?a=2",
       },
       {
         id: 3,
         nome: "Caneca Nasa",
-        valor: 110.90,
-        foto: "https://picsum.photos/260/200?a=3"
+        valor: 110.9,
+        foto: "https://picsum.photos/260/200?a=3",
       },
       {
         id: 4,
         nome: "Camiseta SpaceX",
-        valor: 119.50,
-        foto: "https://picsum.photos/260/200?a=4"
+        valor: 119.5,
+        foto: "https://picsum.photos/260/200?a=4",
       },
       {
         id: 5,
         nome: "Miniatura Starship",
-        valor: 597.90,
-        foto: "https://picsum.photos/260/200?a=5"
+        valor: 597.9,
+        foto: "https://picsum.photos/260/200?a=5",
       },
       {
         id: 6,
         nome: "Miniatura Crew Dragon",
-        valor: 805.90,
-        foto: "https://picsum.photos/260/200?a=6"
+        valor: 805.9,
+        foto: "https://picsum.photos/260/200?a=6",
       },
       {
         id: 7,
         nome: "Mochila Blue Origin",
-        valor: 85.50,
-        foto: "https://picsum.photos/260/200?a=7"
+        valor: 85.5,
+        foto: "https://picsum.photos/260/200?a=7",
       },
       {
         id: 8,
         nome: "Boné Blue Origin",
-        valor: 55.50,
-        foto: "https://picsum.photos/260/200?a=8"
-      }
+        valor: 55.5,
+        foto: "https://picsum.photos/260/200?a=8",
+      },
     ],
 
     inputValorMinimo: "",
     inputValorMaximo: "",
     inputBuscarProduto: "",
+    filtroSelecionado: "Crescente",
 
-    carrinhoDeCompras: false
-
+    carrinhoDeCompras: false,
   };
 
-  onChangeValorMinimo = event => {
-    this.setState({inputValorMinimo: Number(event.target.value)});
+  compararNumeros = (a, b) => {
+    return a.valor - b.valor;
   };
 
-  onChangeValorMaximo = event => {
-    this.setState({inputValorMaximo: Number(event.target.value)});
+  compararNumeros2 = (a, b) => {
+    return b.valor - a.valor;
   };
 
-  onChangeBuscarProduto = event => {
-    this.setState({inputBuscarProduto: event.target.value.toUpperCase()});
+  alteraFiltro = (event) => {
+    this.setState({ filtroSelecionado: event.target.value });
+    console.log(this.state);
+  };
+
+  onChangeValorMinimo = (event) => {
+    this.setState({ inputValorMinimo: Number(event.target.value) });
+  };
+
+  onChangeValorMaximo = (event) => {
+    this.setState({ inputValorMaximo: Number(event.target.value) });
+  };
+
+  onChangeBuscarProduto = (event) => {
+    this.setState({ inputBuscarProduto: event.target.value.toUpperCase() });
   };
 
   acessaCarrinho = () => {
-    let mudaCarrinho = !this.state.carrinhoDeCompras
-    this.setState({carrinhoDeCompras: mudaCarrinho})
+    let mudaCarrinho = !this.state.carrinhoDeCompras;
+    this.setState({ carrinhoDeCompras: mudaCarrinho });
   };
 
-  render () {
+  render() {
+    let listaOrdenada;
+    switch (this.state.filtroSelecionado) {
+      case "Crescente":
+        listaOrdenada = this.state.listaDeProdutos.sort(this.compararNumeros);
+        break;
+      case "Decrescente":
+        listaOrdenada = this.state.listaDeProdutos.sort(this.compararNumeros2);
+      default:
+    }
 
-    let listaFiltrada = this.state.listaDeProdutos;
+    let listaFiltrada = listaOrdenada;
 
     if (this.state.inputValorMinimo) {
-      listaFiltrada = listaFiltrada.filter(produto => {
-        return produto.valor >= this.state.inputValorMinimo
-      })
+      listaFiltrada = listaFiltrada.filter((produto) => {
+        return produto.valor >= this.state.inputValorMinimo;
+      });
     }
 
     if (this.state.inputValorMaximo) {
-      listaFiltrada = listaFiltrada.filter(produto => {
-        return produto.valor <= this.state.inputValorMaximo
-      })
+      listaFiltrada = listaFiltrada.filter((produto) => {
+        return produto.valor <= this.state.inputValorMaximo;
+      });
     }
 
     if (this.state.inputBuscarProduto) {
-      listaFiltrada = listaFiltrada.filter(produto => {
-        return produto.nome.toUpperCase() === this.state.inputBuscarProduto
-      })
+      listaFiltrada = listaFiltrada.filter((produto) => {
+        return produto.nome.toUpperCase() === this.state.inputBuscarProduto;
+      });
     }
 
-    listaFiltrada = listaFiltrada.map(produto => {
+    listaFiltrada = listaFiltrada.map((produto) => {
       return (
         <div>
           <Produtos
@@ -164,14 +190,13 @@ class App extends React.Component {
             nomeProduto={produto.nome}
             valorProduto={produto.valor}
             fotoProduto={produto.foto}
-          />         
+          />
         </div>
-      )
-    }); 
+      );
+    });
 
     return (
       <AppContainer>
-        
         <Header>
           <h1>Loja Space</h1>
           <h3>Filtros:</h3>
@@ -192,25 +217,23 @@ class App extends React.Component {
             value={this.state.inputBuscarProduto}
             onChange={this.onChangeBuscarProduto}
           />
+          <ContainerSelect>
+            <label>Quantidade de produtos: 6 >> </label>
+            <select
+              value={this.state.filtroSelecionado}
+              onChange={this.alteraFiltro}
+            >
+              <option value="Crescente">Preço crescente</option>
+              <option value="Decrescente">Preço decrescente</option>
+            </select>
+          </ContainerSelect>
         </Header>
 
-        <ContainerProdutos>
-          {listaFiltrada}
-        </ContainerProdutos>
+        <ContainerProdutos>{listaFiltrada}</ContainerProdutos>
 
         <ContainerCarrinho>
           <Imagem src={carrinho} onClick={this.acessaCarrinho}></Imagem>
         </ContainerCarrinho>
-        
-//         Abaixo, o que estava na master dando conflito com a branch. Não sabia se era importante, por isso mantive aquu
-//         Se não for importante, é só tirar até o componente  </ Container>
-        <Container>
-          <Filtros />
-          <AppProdutos>
-            {listaRenderizada}
-          </AppProdutos>
-          <Carrinho />
-        </ Container>
       </AppContainer>
     );
   }
